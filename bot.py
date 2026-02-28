1cccee56959ce3fec257e650af9c74a3e4894562
smartcellimports11-wq <smartcellimports11@gmail.com>
2026-02-27 21:57:50 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,17 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from crm import exemplo_crm  
 
-TOKEN = os.getenv(8681665732:AAF4yH7ZsDcbwM9AadyUwCdj9VNQ-cD6VEM)
+import os
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🚀 SmartBot Bank Online!")
 
-def main():
-    application = ApplicationBuilder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    print("Bot iniciado...")
-    application.run_polling()
+    dados = exemplo_crm()
 
-if __name__ == "__main__":
-    main()
+    await update.message.reply_text(f"CRM retornou: {dados}")
+TOKEN = os.getenv("8681665732:AAF4yH7ZsDcbwM9AadyUwCdj9VNQ-cD6VEM")
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
